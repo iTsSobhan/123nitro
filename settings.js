386130e466c91e30dcace81b87a99e1aa6767d8f
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-10-24 20:27:55 +0330

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
 exports.DISCORD_CHANNEL_ID = "883319384750301234";/* Your Discord server's Channel ID,please change it with desired channel that you want.Bot will message valid Discord Nitros when found. */
 
-exports.BOT_TOKEN = "ODYwNzcwNTg1MjQ0MDczOTg0.YOAFEw.BU8HRzxbHFaFCIp1rOUhBC8TW3Q"/* Your bot's login token information please change it.*/
+exports.BOT_TOKEN = ""/* Your bot's login token information please change it.*/
