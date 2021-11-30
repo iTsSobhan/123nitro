d4b5636dd835baee1bd33e80a727dfb510af77bc
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-11-30 03:44:44 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,119 +1,55 @@
-/*
-           ▄████████ ███▄▄▄▄       ███      ▄█  
-          ███    ███ ███▀▀▀██▄ ▀█████████▄ ███  
-          ███    ███ ███   ███    ▀███▀▀██ ███▌ 
-          ███    ███ ███   ███     ███   ▀ ███▌
-        ▀███████████ ███   ███     ███     ███▌
-          ███    ███ ███   ███     ███     ███ 
-          ███    ███ ███   ███     ███     ███ 
-          ███    █▀   ▀█   █▀     ▄████▀   █▀  
+const { Client, RichEmbed, discord } = require("discord.js");
+const Discord = require('discord.js');
 
-        
-    use responsibly it can get you blocked from discord.
 
-*/
-const SETTINGS = require('./settings.js'); // Imports the settings.
-const request = require('request'); // for http request.
-const logger = require(__dirname + "/util/logger"); // logs working nitros.
-const fs = require("fs"); // filesystem.
-const Discord = require('discord.js')
-const client = new Discord.Client();
-const proxyFile = __dirname + "/proxies.txt" // proxy list declaration.
-const triesPerSecond = 1;
-var proxyLine = 0;
-var proxyURL = "https://";
-var working = []; // Arrays.
 
-logger.info("Nitro Generator by Sobhan.SRZA#2153");
+const client = new Client();
+const sezar = require (procsess.evn)
+const prefix = sezar.PREFIX
+client.login(procsess.evn.TOKEN)
 
-client.on('ready', message => {
-    client.channels.cache.get(SETTINGS.DISCORD_CHANNEL_ID).send('dar shoroe amaliat👀');
-})
+function nitroCodeGen() {
+   var length = 18;
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
 
-client.on('message', msg => {
-    if (msg.content === '.gen')
-        msg.reply('ridy nitro nadarim🤣');
+client.on("ready", () => {
+  client.user.setStatus("dnd")
+ console.log("ready!")
+
 });
+client.on("ready", () => {
+
+   client.user.setActivity(`${prefix}gen`, { type: "WATCHING"})
+});
+
 client.on('message', msg => {
-           
-    msg = function () {
-        try {
-            let code = "";
-            let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            for (var i = 0; i < 24; i++)
-                code = code + letters.charAt(Math.floor(Math.random() * letters.length));
-            return code;
-        }
-        catch (error) { }
-    }
-    function updateLine() {
-        try {
-            proxyLine++
-            var lineReader = require('line-reader');
-            var readLine = 0;
-            lineReader.eachLine(proxyFile, function (line, last) {
-                readLine++;
-                if (readLine === proxyLine)
-                    proxyURL = "http://" + line;
-                if (last)
-                    readLine = 0;
-            })
-        }
-        catch (error) { }
-    }
-    updateLine();
-    msg = function (code) {
-        try {
-            var proxiedRequest = request.defaults({ 'proxy': proxyURL });
-            proxiedRequest.timeout = 1500;
-            proxiedRequest.get(`https://discordapp.com/api/v6/entitlements/gift-codes/${code}?with_application=false&with_subscription_plan=true`, (error, resp, body) => {
-                if (error) {
-                    console.log('\x1b[33m%s\x1b[0m', `Connection error: switching proxy`);
-                    updateLine();
-                    return;
-                }
+  if (msg.content === `${prefix}gen`) {
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());  
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+    msg.channel.send("https://discord.gift/" + nitroCodeGen());
+             
+    msg.reply('nitro overloaded pls use the cmd again!')
+    
 
-                body = JSON.parse(body);
-                if (body.message != "Unknown Gift Code" && body.message != "You are being rate limited.") {
-                    body.message.send(`FOUND CODE THAT WORKS: https://discord.gift/${code}`);
-                    logger.log('\x1b[41m', `FOUND CODE THAT WORKS: https://discord.gift/${code}`);
-                    console.log(JSON.stringify(body, null, 4));
-                    working.push(`https://discord.gift/${code}`);
-                    fs.writeFileSync(__dirname + '/codes.json', JSON.stringify(working, null, 4));
-                }
-                else if (body.message === "You are being rate limited.") {
-                    client.on('ready', () => {
-                        body.channelX.send('Rate limit reached! Switching proxy.')
-                    })
-                    updateLine();
-                    console.log("Rate limit reached! Switched proxy");
-
-                } else {
-                    console.log('\x1b[36m%s\x1b[0m', `Invalid: ${code} : Searching!`);
-                }
-            });
-        }
-        catch (error) {
-            logger.error(`An error occurred:`);
-            logger.error(error);
-            return;
-        }
-    }
-    if (msg.content === '.gen') {
-        try {
-            msg.channel.send('shoroe kar🤠')
-            msg(msg());
-            setInterval(() => {
-                msg(msg());
-            }, (5 / triesPerSecond) * 50);
-            setInterval(() => {
-                msg.channel.send('bale haji darhal karam😀')
-            }, 360);
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }
-})
-// made by anti with love <3
-client.login(SETTINGS.BOT_TOKEN);
+  }
+});
