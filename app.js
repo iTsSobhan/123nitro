fb04471136b8a0bbd1b0133200d1a2b41ea13e90
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-12-29 02:15:14 +0330

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ client.on("ready", () => {
    console.log("bote kharkosat run shod seyed")
 });
 
+// const   chars = ['a', 'b', 'c', 'd',  'e','f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+  '1','2','3','4','5','6','7','8','9','0'
+  ];
+
 client.on('message', msg => {
 
   if (channelIds.includes(message.channel.id)) {
