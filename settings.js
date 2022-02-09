f3a76528b988b3bafe489cc45e461ffff8ee223e
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-02-09 11:55:39 +0330

    Delete settings.js

--- settings.js
+++
@@ -1,3 +0,0 @@
-exports.DISCORD_CHANNEL_ID = "";/* Your Discord server's Channel ID,please change it with desired channel that you want.Bot will message valid Discord Nitros when found. */
-
-exports.BOT_TOKEN = ""/* Your bot's login token information please change it.*/
